0e17a3d7c57c6565bc4824f7ceda6d9938dae418
Mateus Ribeiro Daniel <mateusribeirodaniel@hotmail.com>
2022-11-18 10:04:34 -0300

    js perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -17,11 +17,29 @@ const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
   const elemento = document.getElementById(parametro);
-  if (elemento){
+  if (elemento) {
     elemento.checked = true;
   }
 }
 
 parametros.forEach(ativarProduto);
 
-console.log(parametros);
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
